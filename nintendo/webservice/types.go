e96b303305791c691f3d4ac3ac36376f1b6b7ad0
mxha39 <mohamed@mxha.de>
2025-08-30 23:12:05 +0200

    use helper method to send requests

--- nintendo/webservice/types.go
+++ nintendo/webservice/types.go
@@ -1,36 +1,40 @@
 package webservice
 
-type GetWebServiceTokenRequest struct {
-	Parameter struct {
+type (
+	GetWebServiceTokenRequest struct {
+		Parameter GetWebServiceTokenRequestParameter `json:"parameter"`
+	}
+
+	GetWebServiceTokenRequestParameter struct {
 		F                 string `json:"f"`
 		Id                int64  `json:"id"`
 		RegistrationToken string `json:"registrationToken"`
 		RequestId         string `json:"requestId"`
 		Timestamp         int64  `json:"timestamp"`
-	} `json:"parameter"`
-}
+	}
 
-type GetWebServiceTokenResponse struct {
-	Status int `json:"status"`
-	Result struct {
-		AccessToken string `json:"accessToken"`
-		ExpiresIn   int    `json:"expiresIn"`
-	} `json:"result"`
-	CorrelationId string `json:"correlationId"`
-}
+	GetWebServiceTokenResponse struct {
+		Status int `json:"status"`
+		Result struct {
+			AccessToken string `json:"accessToken"`
+			ExpiresIn   int    `json:"expiresIn"`
+		} `json:"result"`
+		CorrelationId string `json:"correlationId"`
+	}
 
-type ListWebServicesResponse struct {
-	Status int `json:"status"`
-	Result []struct {
-		Id               int64  `json:"id"`
-		Uri              string `json:"uri"`
-		CustomAttributes []struct {
-			AttrValue string `json:"attrValue"`
-			AttrKey   string `json:"attrKey"`
-		} `json:"customAttributes"`
-		WhiteList []string `json:"whiteList"`
-		Name      string   `json:"name"`
-		ImageUri  string   `json:"imageUri"`
-	} `json:"result"`
-	CorrelationId string `json:"correlationId"`
-}
+	ListWebServicesResponse struct {
+		Status int `json:"status"`
+		Result []struct {
+			Id               int64  `json:"id"`
+			Uri              string `json:"uri"`
+			CustomAttributes []struct {
+				AttrValue string `json:"attrValue"`
+				AttrKey   string `json:"attrKey"`
+			} `json:"customAttributes"`
+			WhiteList []string `json:"whiteList"`
+			Name      string   `json:"name"`
+			ImageUri  string   `json:"imageUri"`
+		} `json:"result"`
+		CorrelationId string `json:"correlationId"`
+	}
+)
--- nintendo/webservice/webservice_token.go
+++ nintendo/webservice/webservice_token.go
@@ -4,57 +4,40 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"nintendo-api/nxapi"
+	"nintendo-api/utils"
 )
 
-func GetWebServiceToken(webServiceId int64, fGenerationResponse nxapi.FGenerationResponse, accessToken string) (GetWebServiceTokenResponse, error) {
+func GetWebServiceToken(webServiceId int64, fGenerationResponse nxapi.FGenerationResponse, accessToken string) (*GetWebServiceTokenResponse, error) {
 
 	config, err := nxapi.GetConfig()
 	if err != nil {
-		return GetWebServiceTokenResponse{}, fmt.Errorf("failed to get config: %w", err)
+		return nil, fmt.Errorf("failed to get config: %w", err)
 	}
 	configVersion := config.Versions[0]
 
-	webServiceTokenRequest := GetWebServiceTokenRequest{}
-	webServiceTokenRequest.Parameter.F = fGenerationResponse.F
-	webServiceTokenRequest.Parameter.Timestamp = fGenerationResponse.Timestamp
-	webServiceTokenRequest.Parameter.RequestId = fGenerationResponse.RequestId
-	webServiceTokenRequest.Parameter.Id = webServiceId
-	webServiceTokenRequest.Parameter.RegistrationToken = accessToken
-
-	webServiceTokenRequestJson, err := json.Marshal(webServiceTokenRequest)
+	webServiceTokenRequestJson, err := json.Marshal(GetWebServiceTokenRequest{
+		Parameter: GetWebServiceTokenRequestParameter{
+			F:                 fGenerationResponse.F,
+			Timestamp:         fGenerationResponse.Timestamp,
+			RequestId:         fGenerationResponse.RequestId,
+			Id:                webServiceId,
+			RegistrationToken: accessToken,
+		},
+	})
 	if err != nil {
-		return GetWebServiceTokenResponse{}, fmt.Errorf("failed to marshal json: %w", err)
+		return nil, fmt.Errorf("failed to marshal json: %w", err)
 	}
 
 	request, err := http.NewRequest("POST", nintendoBaseUrl+"/v2/Game/GetWebServiceToken", bytes.NewBuffer(webServiceTokenRequestJson))
 	if err != nil {
-		return GetWebServiceTokenResponse{}, fmt.Errorf("failed to create request: %w", err)
+		return nil, fmt.Errorf("failed to create request: %w", err)
 	}
 	request.Header.Set("X-Platform", configVersion.Platform)
 	request.Header.Set("X-ProductVersion", configVersion.Version)
 	request.Header.Set("Authorization", "Bearer "+accessToken)
 	request.Header.Set("Content-Type", "application/json")
 	request.Header.Set("User-Agent", fmt.Sprintf("com.nintendo.znca/%s (Android/14)", config.NsoVersion))
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		return GetWebServiceTokenResponse{}, fmt.Errorf("failed to send request: %w", err)
-	}
-	defer response.Body.Close()
-	if response.StatusCode != 200 {
-		return GetWebServiceTokenResponse{}, fmt.Errorf("status code: %d", response.StatusCode)
-	}
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return GetWebServiceTokenResponse{}, fmt.Errorf("failed to read response: %w", err)
-	}
-	var getWebServiceTokenResponse GetWebServiceTokenResponse
-	err = json.Unmarshal(body, &getWebServiceTokenResponse)
-	if err != nil {
-		return GetWebServiceTokenResponse{}, fmt.Errorf("failed to unmarshal response: %w", err)
-	}
-	return getWebServiceTokenResponse, nil
+	return utils.DoReq[GetWebServiceTokenResponse](request)
 }
